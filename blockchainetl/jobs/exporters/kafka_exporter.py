6be9a2f4dc0926320a6cd82e2e1807f82eab8c6e
wonderbeat <borov.htid@gmail.com>
2024-08-06 10:21:02 +0000

    KafkaItemExporter env parameters

--- blockchainetl/jobs/exporters/kafka_exporter.py
+++ blockchainetl/jobs/exporters/kafka_exporter.py
@@ -1,11 +1,12 @@
 import collections
 import json
 import logging
+import os
+from typing import Any
 
 from kafka import KafkaProducer
 
 from blockchainetl.jobs.exporters.converters.composite_item_converter import CompositeItemConverter
-
 
 class KafkaItemExporter:
 
@@ -14,7 +15,21 @@ class KafkaItemExporter:
         self.converter = CompositeItemConverter(converters)
         self.connection_url = self.get_connection_url(output)
         print(self.connection_url)
-        self.producer = KafkaProducer(bootstrap_servers=self.connection_url)
+        kafka_params = self.read_kafka_env_vars()
+        kafka_params["bootstrap_servers"] = self.connection_url
+        self.producer = KafkaProducer(**kafka_params)
+
+    def read_kafka_env_vars(self):
+        """Reads environment variables starting with "kafka_" and returns a dictionary."""
+        kafka_env_vars = {}
+        for key, value in os.environ.items():
+            try:
+                value = int(value)
+            except ValueError:
+                pass # value is not an int
+            if key.startswith("KAFKA_"):
+                kafka_env_vars[key[len("KAFKA_"):].lower()] = value
+        return kafka_env_vars
 
     def get_connection_url(self, output):
         try:
